8a0839ccc4c2a92098037d5f2b06498cd638f713
raicherodrigo <rodrigoraiche@outlook.com>
2020-06-03 18:53:07 -0300

    added challenge 05 - First Node.js project

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,45 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
+  }
+
+  public validateValue(valueRec: number, type: 'income' | 'outcome'): boolean {
+    const { income, outcome, total }: Balance = this.getBalance();
+    if (type === 'outcome' && valueRec > total) {
+      return true;
+    }
+    return false;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((sum, currentValue) => {
+      if (currentValue.type === 'income') {
+        return sum + currentValue.value;
+      }
+      return sum;
+    }, 0);
+
+    const outcome = this.transactions.reduce((sum, currentValue) => {
+      if (currentValue.type === 'outcome') {
+        return sum + currentValue.value;
+      }
+      return sum;
+    }, 0);
+
+    const total = income - outcome;
+
+    const balance = { income, outcome, total };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
